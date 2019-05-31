56340bd3a8e396ef6635831b50dd69914da3e98b
fosmjo <imefangjie@gmail.com>
2019-05-31 23:06:13 +0800

    fix: improve users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
-    confirmed_at { nil }
+    # set confirmed_at, so it won't send confirmation email when running tests
+    confirmed_at { Time.now }
   end
 end
