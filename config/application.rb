12fa5a3eecbde9faf82e18a519411c7c7745fd1b
fosmjo <imefangjie@gmail.com>
2019-06-02 17:50:44 +0800

    feat: config action mailer

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,20 @@ module Pastebin
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Action mailer config
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.qq.com',
+        port:                 465,
+        user_name:            Settings.mail.smtp.user_name,
+        password:             Settings.mail.smtp.password,
+        authentication:       'plain',
+        tls: true,
+        enable_starttls_auto: true
+    }
+    config.action_mailer.default_url_options = Settings.mail.default_url_options.to_h
   end
 end
